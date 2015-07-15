52ae0f1325543bebcf0c9f938fc1a8111f0e6f96
edmundlth <edmundlth@yahoo.com>
2015-07-15 12:24:01 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='primer_design',
+    version='0.1.0',
+    author='Edmund Lau',
+    author_email='elau1@student.unimelb.edu.au',
+    packages=['primer_design'],
+    entry_points={
+        'console_scripts': ['primer_design = primer_desig3:main']
+    },
+    url='https://github.com/edmundlth/primer_design',
+    install_requires=[],
+)
